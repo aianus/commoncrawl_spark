f511c952c2bac4f34531fb9362e65a136168a230
Alex Ianus <hire@alexianus.com>
2016-05-26 20:03:13 -0400

    Downgrade to scala 2.10 for EMR

--- build.sbt
+++ build.sbt
@@ -2,18 +2,18 @@ name := "commoncrawl_spark"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.10.6"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1" % "provided"
-libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.2.0" % "provided"
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.7.2" % "provided"
 libraryDependencies += ("com.martinkl.warc" % "warc-hadoop" % "0.1.0").excludeAll(
   ExclusionRule(organization = "org.apache.hadoop")
 )
 libraryDependencies += "org.jsoup" % "jsoup" % "1.9.2"
 libraryDependencies ++= Seq(
   "org.slf4j" % "slf4j-api" % "1.7.10",
-  "org.scala-lang" % "scala-reflect" % "2.11.8",
-  "org.scala-lang" % "scala-compiler" % "2.11.8",
+  "org.scala-lang" % "scala-reflect" % "2.10.6",
+  "org.scala-lang" % "scala-compiler" % "2.10.6",
   "org.apache.commons" % "commons-lang3" % "3.3.2",
   "jline" % "jline" % "2.12.1"
 )
--- src/main/scala/com/alexianus/commoncrawl_spark/CommoncrawlWappalyzer.scala
+++ src/main/scala/com/alexianus/commoncrawl_spark/CommoncrawlWappalyzer.scala
@@ -1,16 +1,24 @@
 package com.alexianus.commoncrawl_spark
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+import java.net.URL
+
+import com.alexianus.wappalyzer.AppDetector
 import com.martinkl.warc._
 import com.martinkl.warc.mapreduce._
 import org.apache.hadoop.io.LongWritable
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Try
 
 object CommoncrawlWappalyzer {
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("CommoncrawlWappalyzer")
     val sc = new SparkContext
+
+    val num_pages = sc.accumulator(0)
+
+
     val warc = sc.newAPIHadoopFile(
       args(0),
       classOf[WARCInputFormat],
@@ -18,10 +26,24 @@ object CommoncrawlWappalyzer {
       classOf[WARCWritable]
     )
 
-    val numPages = warc
-      .map(x => (1,1))
-      .count()
-
-    println("Records in warc: %s".format(numPages))
+    warc
+      .flatMap { case (_, record: WARCWritable) =>
+          val r = record.getRecord
+          if (r.getHeader.getRecordType == "response") Some(r) else None
+      }
+      .flatMap { record =>
+        Try {
+          val domain = new URL(record.getHeader.getTargetURI).getHost
+          val body = new String(record.getContent, "UTF-8")
+          num_pages += 1
+          (domain, AppDetector.detect(body))
+        }.toOption
+      }
+      .combineByKey(
+        identity,
+        (s1: Set[String], s2: Set[String]) => s1.union(s2),
+        (s1: Set[String], s2: Set[String]) => s1.union(s2)
+      )
+      .saveAsTextFile(args(1))
   }
 }
